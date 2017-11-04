b995683d0d59428cc457c72bc11d57fbf37e174c
Stefan Geyer <stefangeyer@outlook.com>
2017-11-04 12:21:27 +0100

    Added argument parsing and error handling for the socket functions

--- src/server.c
+++ src/server.c
@@ -56,19 +56,67 @@ static int connfd = -1;                 // connection file descriptor
  *  - a function which checks whether a client's shot hit any of your ships
  */
 
+static char *pgm_name; /**< The program name */
+
+/**
+ * Mandatory usage function.
+ * @brief This function writes helpful usage information about the program to stderr.
+ * @details global variables: pgm_name
+ */
 void usage(void) {
-    printf("Server:\n"
-                   "\tSYNOPSIS\n"
-                   "\t\tserver [-p PORT] SHIP1...\n"
-                   "\tEXAMPLE\n"
-                   "\t\tserver -p 1280 C2E2 F0H0 B6A6 E8E6 I2I5 H8I8\n");
+    fprintf(stderr, "Server:\n\tSYNOPSIS\n\t\t%s [-p PORT] SHIP1...\n\tEXAMPLE\n"
+            "\t\t%s -p 1280 C2E2 F0H0 B6A6 E8E6 I2I5 H8I8\n", pgm_name, pgm_name);
+    exit(EXIT_FAILURE);
+}
+
+/**
+ * Prints anerror message to stderr. Should only be called whenever an error occurs
+ * shall exit with failure.
+ * @brief This function prints an error message to stderr and exits with EXIT_FAILURE.
+ * @details global variables: pgm_name
+ * @param message The message that shall be printed before exiting
+ */
+void exitError(char *message, bool showUsage) {
+    fprintf(stderr, "%s: An error has occurred: %s\n", pgm_name, message);
+    if (showUsage) usage();
+    else exit(EXIT_FAILURE);
+}
+
+void parseArguments(int argc, char *argv[]) {
+    pgm_name = argv[0];
+
+    int c;
+    int opt_p = 0;
+    bool has_invalid_option = false;
+
+    // 3rd param: optstring defines legitimate option characters.
+    // Appending : to an argument character implicates that the option requires an argument
+    while ((c = getopt(argc, argv, "p:")) != -1) {
+        switch (c) {
+            case 'p':
+                // Option p is optional and may occur once.
+                opt_p++;
+                port = optarg;
+                break;
+            case '?':
+                has_invalid_option = true;
+                break;
+            default:
+                assert(0);
+        }
+    }
+
+    if (opt_p >= 1) exitError("Port option can be set only once", true);
+
+    // Check whether there are the correct amount of positional arguments
+    // or an in valid option were supplied.
+    if ((argc - optind) != 2 || has_invalid_option) usage();
+
 }
 
 int main(int argc, char *argv[]) {
-    /* TODO
-     * Add code to parse the command line arguments, maybe as a separate
-     * function.
-     */
+    // This also handles setting pgm_name
+    parseArguments(argc, argv);
 
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
@@ -76,36 +124,25 @@ int main(int argc, char *argv[]) {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
+    // Returns 0 or error code but does not set errno
     int res = getaddrinfo(NULL, port, &hints, &ai);
-    if (res != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(res));
-    }
+    if (res != 0) exitError((char *) gai_strerror(res), false);
 
     sockfd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
-    if (sockfd < 0) {
-        // error
-    }
+    if (sockfd < 0) exitError(strerror(errno), false);
 
     int val = 1;
     res = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof val);
-    /* TODO
-     * check for errors
-     */
+    if (res) exitError(strerror(errno), false);
 
     res = bind(sockfd, ai->ai_addr, ai->ai_addrlen);
-    /* TODO
-     * check for errors
-     */
+    if (res) exitError(strerror(errno), false);
 
     res = listen(sockfd, 1);
-    /* TODO
-     * check for errors
-     */
+    if (res) exitError(strerror(errno), false);
 
     connfd = accept(sockfd, NULL, NULL);
-    /* TODO
-     * check for errors
-     */
+    if (connfd) exitError(strerror(errno), false);
 
     /* TODO
      * Here you might want to add variables to keep track of the game status,
@@ -125,4 +162,5 @@ int main(int argc, char *argv[]) {
     /* TODO
      * cleanup
      */
+    freeaddrinfo(ai);
 }
